652fb4409d19a1a0838b4bdb0debddf55a9c1687
kazuma1112 <kazuma1112.mainmail@gmail.com>
2024-05-10 12:57:40 +0900

    [fix] 。の削除

--- components/letsget.tsx
+++ components/letsget.tsx
@@ -9,20 +9,20 @@ export function LetsStart() {
         <div className="py-2">
             <div className="bg-white bg-opacity-80 rounded-xl">
                 <div className="p-5 text-center w-full">
-                    <h1 className="text-2xl sm:text-3xl font-bold"> かんたんに、さっそく、すばやくはじめよう。</h1>
+                    <h1 className="text-2xl sm:text-3xl font-bold"> かんたんに、さっそく、すばやくはじめよう</h1>
                     <h2 className="text-2xl sm:text-xl font-bold"> let's gets start! </h2>
                 </div>
                 <div className="px-3 text-xl sm:text-2xl font-semibold py-10 opacity-100"> 
                     <div className="text-gray-800">
                         <div className="pb-4 min-w-full">
-                            <h1><span className="bg-yellow-200 px-1">ダッシュボードから確認してみる。</span></h1>
+                            <h1><span className="bg-yellow-200 px-1">ダッシュボードから確認してみる</span></h1>
                             <p className="px-2 pb-2">APIの使い方がわからない方のために、ダッシュボードを用意しています。まずはこちらからでも！</p>
                             <Link href="/dashboard">
                                 <Button color="primary">ダッシュボードを起動する</Button>
                             </Link> 
                         </div>
                         <div className="pb-4 min-w-full">
-                            <h1><span className="bg-yellow-200 px-1">早速APIを使ってみる。</span></h1>
+                            <h1><span className="bg-yellow-200 px-1">早速APIを使ってみる</span></h1>
                             <p className="px-2 pb-2">APIを使うことはとっても慣れてるという方のために、ドキュメントを用意しています。こちらからどうぞ。</p>
                             <Link href="https://hss-dev-docs.aknet.tech">
                                 <Button color="primary">ドキュメントに移動する</Button>
